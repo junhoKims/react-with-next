2e1e9029b6a44a908896fcaec7172c8a82d10000
junhoKims <kraksin12@gmail.com>
2021-03-06 16:44:47 +0900

    Set global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/app'
-import 'styles/globals.css'
+import Head from 'next/head'
+import { StylesProvider } from '@material-ui/core/styles'
+import { CssBaseline } from '@material-ui/core'
+import globals from 'styles/globals'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    </Head>
+    {globals}
+
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </StylesProvider>
+  </>
 )
 
 export default MyApp
